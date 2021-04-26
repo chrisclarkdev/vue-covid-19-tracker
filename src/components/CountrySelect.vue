0f83fcdcfe55d712e3c9c396f378f474504e04e7
Chris Clark <45742785+chrisclarkdev@users.noreply.github.com>
2021-04-26 22:01:02 +0100

    Add files via upload

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,29 @@
+<template>
+    <select @change="onChange" v-model="selected" 
+    class="form-select-1 block w-full rounded p-3 mt-10">
+    <option value="0">Select Country</option>
+    <option v-for="country in countries" :value="country.ID">
+        {{country.Country}}
+    </option>
+    </select>
+</template>
+
+<script>
+
+export default {
+    name: 'CountrySelect',
+    props: ['countries'], 
+    data() {
+        return {
+            selected: 0 
+        }
+    },
+    methods: {
+        onChange() {
+            const country = this.countries.find((item) => item.ID === this.selected)
+                console.log(country)
+                this.$emit('get-country', country)
+            }
+    }
+}  
+</script>
